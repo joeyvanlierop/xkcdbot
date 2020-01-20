29aebe98df42b9b67e09e2ac404be67cc89e3ec0
Joey Van Lierop <joey.vanlierop@me.com>
2020-01-19 23:08:26 -0800

    Restructured project hierarchy (again) and added tests for the config. Adjusted the regex pattern matching to require the number be prefixed by an exclamation mark (!) or a pound sign (#)

---
+++ tests/test_bot_config.py
@@ -0,0 +1,17 @@
+import unittest
+import praw
+from cfg.config import Config
+
+
+class TestBotConfig(unittest.TestCase):
+    def setUp(self):
+        self.config = Config(config_path="test_config.json", section="test")
+
+    def test_constructor(self):
+        self.assertEqual(self.config.username, "username")
+        self.assertEqual(self.config.password, "password")
+        self.assertEqual(self.config.client_id, "client_id")
+        self.assertEqual(self.config.client_secret, "client_secret")
+        self.assertEqual(self.config.user_agent, "user_agent")
+        self.assertEqual(self.config.subreddits, "subreddit1+subreddit2+subreddit3")
+        self.assertEqual(self.config.closer, "^footer1&nbsp;|&nbsp;footer2&nbsp;|&nbsp;footer3")
