2c804a09deff80f51ce6ee049ca03a3e048892f9
Joey Van Lierop <joey.vanlierop@me.com>
2020-01-19 23:30:01 -0800

    Added documentation to most of the bot class

--- bot/bot.py
+++ bot/bot.py
@@ -21,10 +21,25 @@ from cfg.config import Config
 
 
 class Bot():
+    """
+    Class that contains the methods for running the bot.
+
+    Constructor arguments:
+    :param config_path: Path to the config file within the cfg folder.
+    :param section: Section within the config file which hold the settings for the bot.
+    :param backlisted_path: Path to the database which holds the blacklisted users.
+    """
+
     def __init__(self, config_path="config.json", section="default", blacklisted_path="blacklisted.db"):
         self.config = Config(config_path, section)
 
     def main(self):
+        """
+        Starts running the bot.
+
+        Authenticates a new reddit user.
+        Cycles between running the comment stream function and the inbox stream function.
+        """
         reddit = self.authenticate()
         subreddits = reddit.subreddit(self.config.subreddits)
         comment_stream = subreddits.stream.comments(
@@ -35,7 +50,17 @@ class Bot():
             self.run_comment_stream(reddit, comment_stream)
             self.run_inbox_stream(reddit, inbox_stream)
 
+    def authenticate(self):
+        """Returns a new authenticated reddit user with the credentials from the configuration file."""
+        reddit = praw.Reddit(username=self.config.username,
+                            password=self.config.password,
+                            client_id=self.config.client_id,
+                            client_secret=self.config.client_secret,
+                            user_agent=self.config.user_agent)
+        return reddit
+
     def run_comment_stream(self, reddit, stream):
+        """Resposible for calling all the functions which analyze and respond to comments."""
         for comment in stream:
             if comment is None:
                 break
@@ -55,6 +80,7 @@ class Bot():
                 self.reply(comment, response)
 
     def run_inbox_stream(self, reddit, stream):
+        """Resposible for calling all the functions which analyze and respond to inbox messages and mentions."""
         for message in stream:
             if message is None:
                 break
@@ -83,15 +109,15 @@ class Bot():
     #         blacklisted = config[self.section]["blacklisted"] or []
     #         return username in blacklisted
 
-    def authenticate(self):
-        reddit = praw.Reddit(username=self.config.username,
-                             password=self.config.password,
-                             client_id=self.config.client_id,
-                             client_secret=self.config.client_secret,
-                             user_agent=self.config.user_agent)
-        return reddit
-
     def valid_comment(self, comment):
+        """
+        Determines if a comment is valid.
+
+        An valid comment is one that is not:
+         - A comment posted by this bot.
+         - A comment posted by a blacklisted user.
+         - A comment that is saved.
+        """
         username = self.config.username
         author = comment.author
         saved = comment.saved
@@ -106,6 +132,7 @@ class Bot():
             return True
 
     def find_numbers(self, body):
+        """Finds all numbers that should be analyzed by the bot."""
         numbers = re.findall(r"""(?i)(?x)       # Ignore case, comment mode
                             (?<= ! | \# )       # Must be preceded by an exclamation mark or a pound sign    
                             \d+                 # Matches the following numbers
@@ -113,6 +140,10 @@ class Bot():
         return numbers
 
     def find_comic(self, number):
+        """
+        Finds the json data of the comic with the given number.
+        Returns none if there is no comic with the given number.
+        """
         url = f"http://xkcd.com/{number}/info.0.json"
         response = requests.get(url)
 
@@ -123,6 +154,7 @@ class Bot():
             return config
 
     def format_comment(self, data):
+        """Formats a comics json data into a detailed response and returns it."""
         title = data["title"]
         alt = data["alt"]
         img = data["img"]
@@ -150,6 +182,7 @@ class Bot():
         return inspect.cleandoc(response)
 
     def reply(self, comment, response):
+        """Replies to the given comment with the given response."""
         print(response)
         # comment.reply(response)
         # comment.save()
