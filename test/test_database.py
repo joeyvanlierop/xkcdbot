6769ef7b88d5d7c4172f13e84db9c2120b06586b
Joey Van Lierop <joey.vanlierop@me.com>
2020-01-21 20:16:17 -0800

    Temporarily removed the date parameter for when adding ids to the statistics database.

--- test/test_database.py
+++ test/test_database.py
@@ -17,14 +17,13 @@ class TestDatabase(unittest.TestCase):
         self.assertFalse(self.database.is_blacklisted(""))
 
     def test_statistics(self):
-        date = datetime.utcnow()
         self.database.add_id("abc", "1")
         self.database.add_id("def", "2")
         self.database.add_id("ghi", "2")
-        self.database.add_id("jkl", "3", date)
-        self.database.add_id("mno", "3", date)
-        self.database.add_id("pqr", "3", date)
-        self.database.add_id("stu", "3", date)
+        self.database.add_id("jkl", "3")
+        self.database.add_id("mno", "3")
+        self.database.add_id("pqr", "3")
+        self.database.add_id("stu", "3")
         self.assertEqual(self.database.comic_id_count("0"), 0)
         self.assertEqual(self.database.comic_id_count("1"), 1)
         self.assertEqual(self.database.comic_id_count("2"), 2)
